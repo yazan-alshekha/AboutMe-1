f62402b5c9c96ce0bfb29ab3a418f696b098f993
azez-alhoot <azezalhoot82@yahoo.com>
2020-05-12 21:16:40 +0300

    add new question using for loop and array and lists

--- js/app.js
+++ js/app.js
@@ -1,47 +1,47 @@
 'use strict'
 
+
+var score = 0;
+
 var userName = prompt('What is Your Name?');
 console.log(userName);
-alert('Your Welcome '+ userName + ' In My Web Page');
- 
-var ask = confirm('Okay ' +userName+ ' Let\'s Play a Game! I Will Ask You and Your Answer Shuld be Yes or No Y/N');
+alert('Your Welcome ' + userName + ' In My Web Page');
+
+var ask = confirm('Okay ' + userName + ' Let\'s Play a Game! I Will Ask You and Your Answer Shuld be Yes or No Y/N');
 
 var question1 = prompt('Did I am smoker or no?!')
 var answer1 = question1.toLowerCase();
 
 switch (answer1) {
     case ('yes'):
-        alert('You Right '+userName+' I am Smoker');
-        break;
     case ('y'):
-        alert('You Right '+userName+' I am Smoker');
-        break;    
-    case ('no'):
-        alert('I Hope to stop Smoking Someday, Pray To Me '+userName+' Please!!');
+        alert('You Right ' + userName + ' I am Smoker');
+        score +=1;
         break;
+    case ('no'):
     case ('n'):
-        alert('I Hope to stop Smoking Someday, Pray To Me '+userName+' Please!!');
-        break;    
+        alert('I Hope to stop Smoking Someday, Pray To Me ' + userName + ' Please!!');
+        break;
     default:
         alert('Your Answer Should Be Yes or No Y/N !')
-        break;
-}
+        break;    
+    }
+
+
+
 var question2 = prompt('Do You think I am a Fat Man?')
 var answer2 = question2.toLowerCase();
 
 switch (answer2) {
     case ('yes'):
-        alert('no '+userName+' I am not Fat!');
-        break;
     case ('y'):
-        alert('no '+userName+' I am not Fat!');
-        break;    
-    case ('no'):
-        alert('Yes You Right '+userName+' I am Not fat!');
+        alert('no ' + userName + ' I am not Fat!');
         break;
+    case ('no'):
     case ('n'):
-        alert('Yes You Right '+userName+' I am Not fat!');
-        break;    
+        alert('Yes You Right ' + userName + ' I am Not fat!');
+        score +=1;
+        break;
     default:
         alert('Your Answer Should Be Yes or No Y/N !')
         break;
@@ -52,17 +52,14 @@ var answer3 = question3.toLowerCase();
 
 switch (answer3) {
     case ('yes'):
-        alert('yes Correct '+userName+' I Like Tea!');
-        break;
     case ('y'):
-        alert('yes Correct '+userName+' I Like Tea!');
-        break;    
-    case ('no'):
-        alert('No '+userName+' I Like Tea!');
+        alert('yes Correct ' + userName + ' I Like Tea!');
+        score +=1;
         break;
+    case ('no'):
     case ('n'):
-        alert('No '+userName+' I Like Tea!');
-        break;    
+        alert('No ' + userName + ' I Like Tea!');
+        break;
     default:
         alert('Your Answer Should Be Yes or No Y/N !')
         break;
@@ -73,17 +70,14 @@ var answer4 = question4.toLowerCase();
 
 switch (answer4) {
     case ('yes'):
-        alert('Actually I am Not Good Panter At All '+userName);
-        break;
     case ('y'):
-        alert('Actually I am Not Good Panter At All '+userName);
-        break;    
-    case ('no'):
-        alert('Yes you Right I am Not Good Panter At All '+userName);
+        alert('Actually I am Not Good Panter At All ' + userName);
         break;
+    case ('no'):
     case ('n'):
-        alert('Yes you Right I am Not Good Panter At All '+userName);
-        break;    
+        alert('Yes you Right I am Not Good Panter At All ' + userName);
+        score +=1;
+        break;
     default:
         alert('Your Answer Should Be Yes or No Y/N !')
         break;
@@ -94,20 +88,50 @@ var answer5 = question5.toLowerCase();
 
 switch (answer5) {
     case ('yes'):
-        alert('Thank You '+userName+' But I am Still Learning');
-        break;
     case ('y'):
-        alert('Thank You '+userName+' But I am Still Learning');
-        break;    
-    case ('no'):
-        alert('Okay '+userName+ ' I am Doing My Beast To Be Good Programmer');
+        alert('Thank You ' + userName + ' I hope that');
+        score +=1;
         break;
+    case ('no'):
     case ('n'):
-        alert('Yes you Right I am Not Good Panter At All '+userName);
-        break;    
+        alert('Okay ' + userName + ' I am Doing My Beast To Be Good Programmer');
+        break;
     default:
         alert('Your Answer Should Be Yes or No Y/N !');
         break;
 }
 
-alert('Thank You '+userName+' For Visting My Web Page And Playing With Me, Please Read My Web Page To Know Some Information About Me !');
+var question6 = prompt('How Old Am I ?');
+var answer6 = question6;
+var myAge = 25;
+
+if (answer6 == myAge) {
+    prompt('Great ' + userName + ' I am 25 years old');
+    score +=1;
+} else {
+    for (var i = 0; i < 3; i++) {
+        var newAnswer = prompt('try again');
+        if (newAnswer == myAge) {
+            alert('Great ' + userName + ' I am 25 years old');
+            score +=1;
+            i += 4;
+        }
+    }
+}
+
+
+
+var programingLangusges = ['html', 'css', 'javascript'];
+
+for (var i = 0; i < 6; i++) {
+    var question7 = prompt('Which programming language do you think I used to develop this web site?');
+    var t = programingLangusges.includes(question7);
+    if(t == true){
+        alert('Good Answer '+userName+' I used '+programingLangusges.join(' and ')+' To Produce My web Page');
+        score +=1;
+        i +=6;
+    }
+}
+
+alert(userName+' you got '+score+' out of 7');
+alert('Thank You ' + userName + ' For Visting My Web Page And Playing With Me, Please Read My Web Page To Know Some Information About Me !');
